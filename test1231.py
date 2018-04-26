15446e239471be88951c2f05431da1ab990eae36
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-04-25 21:09:03 -0400

    final changes

--- test1231.py
+++ test1231.py
@@ -4,9 +4,15 @@ second test on python
 """
 
 empname,empno, sal, dept='praveen',1231,30000,'IT'
+com=complex(3,4)
 
 print(empname)
 print(empno)
 print(sal)
 print(dept)
 print("final changes")
+type(empname)
+id(empno)
+print(type(empname))
+print(com.conjugate())
+print(com.real)
